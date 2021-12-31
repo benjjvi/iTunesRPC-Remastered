caa01fca157a5d8159be6dd16016100e9dd3000d
Ben <ben.voisey2@gmail.com>
2021-12-31 18:10:29 +0000

    staging 1: add images to local dict.
    staging 1: add images to local dict, and add some to static.

--- images/grabImages.py
+++ images/grabImages.py
@@ -1,42 +1,37 @@
 # encoding: utf-8
+
 import win32com.client
 import os, time
 
-o = win32com.client.gencache.EnsureDispatch("iTunes.Application") #connect to the COM of iTunes.Application
-
-try:
-    f = open("dict", "r", encoding="utf-8")
-    dictionary = eval(f.read())
-    f.close()
-except Exception:
-    dictionary = {}
+f = open("dict", "r", encoding="utf-8")
+dictionary = eval(f.read())
+f.close()
 
 run = True
-i = int(input("Start from what number? This will be the number that the first image is saved as e.g entering 34 will save 34.png\n> "))
-to = int(input("Continue counting untill what number?\n> "))
+i = int(input("Start from what number? This will be the number that the first image is saved as e.g entering 34 will save 34.png\n> ")) - 1
+toval = (int(input("How many songs are there to count?\n> ")) + i) - 1
 
-print("Starting to collect data in 10 seconds. Be prepared.")
-time.sleep(10)
-
+print("Getting songs in 3 seconds.")
+time.sleep(3)
+o = win32com.client.gencache.EnsureDispatch("iTunes.Application") #connect to the COM of iTunes.Application
 while run:
-    print("Getting first item.")
+    i += 1
     path = (os.path.dirname(os.path.realpath(__file__))).replace("\\", "\\\\") + "\\" + str(i) + ".png"
+
     artwork = o.CurrentTrack.Artwork.Item(1).SaveArtworkToFile(path)
 
     track = o.CurrentTrack.Name
     artist = o.CurrentTrack.Artist
+
     key = track + ":" + artist
     dictionary[key] = i
 
-    print("Saved "+ track +" by "+ artist +" with dictionary key "+ key +" with value "+ str(i))
-    i += 1
-    time.sleep(4)
-    if i == to:
+    print("Got artwork for " + key + " with value " + str(i))
+    if i >= toval:
         run = False
-    
+    else: 
+        time.sleep(4)
 
 f = open("dict", "w", encoding="utf-8")
 f.write(str(dictionary))
 f.close()
-
-print("Completed! Please copy the file named 'dict' to the iTunesRPC-Remastered folder.")
