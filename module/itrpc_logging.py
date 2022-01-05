835f6a47aeb9d9b0f43d6a588279b83597fc3f9a
Ben <ben.voisey2@gmail.com>
2022-01-05 18:30:35 +0000

    bug fix
    fix issue where if a error message (AttributeError type) was passed, program would crash.

--- module/itrpc_logging.py
+++ module/itrpc_logging.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 def log_message(message_as_text):
-    print(message_as_text)
-    message_as_text = message_as_text + "\n"
+    print(str(message_as_text))
+    message_as_text = str(message_as_text) + "\n"
     with open("log", "a+", encoding="utf-8") as f:
         f.write(message_as_text)
